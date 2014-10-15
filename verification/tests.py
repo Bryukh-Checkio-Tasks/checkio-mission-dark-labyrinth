eea7df54c7ec062b86a5db428645285af4ec7a97
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-10-15 16:56:09 +0400

    simple test

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,17 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
-    "Basics": [
-        {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
-        },
-        {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
-    ],
-    "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
-    ]
+    "0. Simple": {
+        "maze": [
+            "XXXXXXX",
+            "X.....X",
+            "X.X.X.X",
+            "X.....X",
+            "X.X.X.X",
+            "X.X.E.X",
+            "XXXXXXX",
+        ],
+        "player": [1, 1]
+    }
 }
